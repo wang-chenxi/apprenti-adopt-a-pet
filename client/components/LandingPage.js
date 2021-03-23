cdda70554e400c4517d2259db3a26192c926c5f6
Chenxi Wang <wangchenxi.us@gmail.com>
2021-03-23 14:10:33 -0700

    Add LandingPage.js

---
+++ client/components/LandingPage.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+const LandingPage = props => {
+    return (<div>
+        <ul>
+            <li>
+                <a href="/types/1">Pigs</a>
+                <img src="https://i.pinimg.com/originals/b4/26/3c/b4263cb1323dc96856c9fc1a50e385ce.png"></img>
+            </li>
+            <li>
+                <a href="/types/2">Bunnies</a>
+                <img src="http://www.lovethispic.com/uploaded_images/350018-Cute-Bunny.jpg"></img>
+            </li>
+            <li>
+                <a href="/types/3">Unicorn</a>
+                <img src="https://www.brit.co/media-library/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbWFnZSI6Imh0dHBzOi8vYXNzZXRzLnJibC5tcy8yMTY0NDE3NC9vcmlnaW4uanBnIiwiZXhwaXJlc19hdCI6MTY3MDY0MjY4M30.RILUqVDTvnD95yDm-1ii6wWLql7x9U-lGBzSJpxr-2I/image.jpg"></img>
+            </li>
+        </ul>
+    </div>)
+}
+
+export default LandingPage
