7bd35d082470a81c1bf95ac656d6f08a71773889
kpartain <karalynnleann@gmail.com>
2021-03-24 07:49:25 -0700

    Initalize root and client routers, getClientIndexPath file
    We may need to consider how to handle surrender form API and creating a router for it as discussed. In this file we indicate that API calls are handled by express and React will handle the page routes including dynamic type routing and routing by id.

--- server/routes/rootRouter.js
+++ server/routes/rootRouter.js
@@ -1,9 +1,14 @@
 import express from "express"
 import clientRouter from "./clientRouter.js"
+import typeRouter from "./typeRouter.js"
+import petRouter from "./petRouter.js"
+import surrenderRouter from "./surrenderRouter.js"
 
 const rootRouter = new express.Router()
 
-rootRouter.use()
+rootRouter.use("api/v1/types", typeRouter)
+rootRouter.use("api/v1/pets", petRouter) 
+rootRouter.use("api/v1/surrender", surrenderRouter)
 
 rootRouter.use("/", clientRouter)
 
