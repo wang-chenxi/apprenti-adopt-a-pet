2f9172fc5fd8b5d9ce47befa83916ab66995bd6a
MatthewMereba <mattacognition@gmail.com>
2021-03-25 14:33:56 -0700

    create Show Page

---
+++ client/components/ShowPage.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react"
+
+const ShowPage = (props) => {
+  const [pet, setPet] = useState({})
+  const [showForm, setShowForm] = useState(false)
+
+  const showForm = () => {
+    setShowForm(!showForm)
+  }
+
+  const getPet = async () => {
+    try {
+      const petId = props.match.params.id
+      const response = await fetch(`/api/v1/${type}/${petId}`)
+      if (!response.ok) {
+        const errorMessage = `${response.status} (${response.statusText})`
+        const error = new Error(errorMessage)
+        throw(error)
+      }
+      const responseBody = await response.json()
+      setPet(responseBody.pet)
+    } catch(err) {
+      console.error(`Pet not found: ${err.message}`)
+    }
+  }
+
+  useEffect(() => {
+    getPet()
+  }, [])
+
+  return (
+    <>
+      <header>
+      <img>{this.imgUrl}</img>
+      </header>
+      <h1>{pet.name}</h1>
+      <h2>{pet.age}</h2>
+      <h2>{pet.vaccinationStatus}</h2>
+      <h2>{pet.adoptionStory}</h2>
+      
+      <div>
+      <form>
+        <button onClick={showForm}>Adopt</button>
+      </form>
+
+      {showForm && (
+        <form>
+          ...
+        </form>
+      )}
+    </div>
+    </>
+    
+  )
+}
+
+
+export default ShowPage
--- server/models/Animal.js
+++ server/models/Animal.js
@@ -1,24 +1,12 @@
 import pg from "pg"
 import _ from "lodash"
+
 const pool = new pg.Pool({
   connectionString: "postgres://postgres:password@localhost:5432/cc_pets"
 })
+
 class Animal {
-  constructor({
-    id,
-    name,
-    img_url,
-    imgUrl,
-    age,
-    vaccination_status,
-    vaccinationStatus,
-    adoption_story,
-    adoptionStory,
-    adoption_status,
-    adoptionStatus,
-    type_id,
-    typeId
-  }) {
+  constructor({ id, name, img_url, imgUrl, age, vaccination_status, vaccinationStatus, adoption_story, adoptionStory, adoption_status, adoptionStatus, type_id, typeId }) {
     this.id = id
     this.name = name
     this.imgUrl = img_url || imgUrl
@@ -28,73 +16,98 @@ class Animal {
     this.adoptionStatus = adoption_status || adoptionStatus
     this.typeId = type_id || typeId
   }
-  static async findByType(typeId) {
+
+
+  static async findByType(type_id) {
     try {
       const client = await pool.connect()
-      const result = await client.query("SELECT * FROM adoptable_pets WHERE type_id = $1", [typeId])
+      const result = await client.query("SELECT * FROM adoptable_pets WHERE type_id = $1", [type_id])
       const allOfTypeData = result.rows[0]
-      const allOfType = allOfTypeData.map(ofType => {
+      const allOfType = allOfTypeData.map((ofType) => {
         return new this(ofType)
       })
+
       client.release()
+
       return allOfType
+
     } catch (error) {
       console.error(error)
       pool.end()
     }
   }
+
   static async findById(id) {
     try {
       const client = await pool.connect()
       const result = await client.query("SELECT * FROM adoptable_pets WHERE id = $1", [id])
       const specificPetData = result.rows[0]
       const specificPet = new this(specificPetData)
+
       client.release()
+
       return specificPet
+
     } catch (error) {
       console.error(error)
       pool.end()
     }
   }
+
   async saveAdoptRequest() {
     try {
       const client = await pool.connect()
-      const query =
-        "INSERT INTO adoptable_pets (name, image_url, age, vaccination_status, adoption_status, type_id) VALUES ($1, $2, $3, $4, $5, $6)"
-      const values = [
-        this.name,
-        this.imageUrl,
-        this.age,
-        this.vaccinationStatus,
-        this.adoptionStatus,
-        this.typeId
-      ]
+      const query = "INSERT INTO adoption_applications (name, phone_number, email, home_status) VALUES ($1, $2, $3, $4)"
+      const values = [this.name, this.phoneNumber, this.email, this.homeStatus]
       await client.query(query, values)
-      const result = await client.query("SELECT * FROM adoptable_pets ORDER BY id DESC LIMIT 1")
-      const newPet = result.rows[0]
-      this.id = newPet.id
+
+      const result = await client.query("SELECT * FROM adoption_applications ORDER BY id DESC LIMIT 1")
+      const newAdoption = result.rows
+
+      this.id = newAdoption.id
+
       client.release()
+
       return true
+
     } catch (error) {
       console.error(error)
       pool.end()
       return false
     }
   }
+
+  async saveSurrender() {
+    try {
+      const client = await pool.connect()
+      const query = "INSERT INTO pet_surrender_applications (name, phone_number, email, pet_name, pet_age, pet_type_id) VALUES ($1, $2, $3, $4, $5, $6)"
+      const values = [this.name, this.phoneNumber, this.email, this.petName, this.petAge, this.petTypeId]
+      await client.query(query, values)
+
+      const result = await client.query("SELECT * FROM surrender_applications ORDER BY id DESC LIMIT 1")
+      const newSurrender = result.rows[0]
+
+      this.id = newSurrender.id
+
+      client.release()
+
+      return true
+
+    } catch (error) {
+      console.error(error)
+      pool.end()
+      return false
+    }
+  }
+
   isValid() {
     this.errors = {}
-    const requiredFields = [
-      "name",
-      "imgUrl",
-      "age",
-      "vaccinationStatus",
-      "adoptionStatus",
-      "typeId"
-    ]
+    const requiredFields = ["name", "image_url", "age", "vaccination_status", "adoption_status", "type_id"]
     let isValid = true
-    for (const requiredField of requiredFields) {
+
+    for(const requiredField of requiredFields) {
       this.errors[requiredField] = []
-      if (!this[requiredField]) {
+      if(!this[requiredField]) {
         isValid = false
         this.errors[requiredField].push("Can't be blank")
       }
@@ -102,4 +115,5 @@ class Animal {
     return isValid
   }
 }
+
 export default Animal
