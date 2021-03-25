54676975a9d7c98f20686b909749d5dafd587676
kpartain <karalynnleann@gmail.com>
2021-03-25 13:35:52 -0700

    Correct path names in router and stage front end to merge with show pages and adopt form

--- server/routes/rootRouter.js
+++ server/routes/rootRouter.js
@@ -1,13 +1,13 @@
 import express from "express"
 import clientRouter from "./clientRouter.js"
-// import typeRouter from "./typeRouter.js"
-// import petRouter from "./petRouter.js"
-import surrenderRouter from "./surrenderRouter.js"
+import typeRouter from "./api/v1/typeRouter.js"
+import petRouter from "./api/v1/petRouter.js"
+import surrenderRouter from "./api/v1/surrenderRouter.js"
 
 const rootRouter = new express.Router()
 
-// rootRouter.use("api/v1/types", typeRouter)
-// rootRouter.use("api/v1/pets", petRouter)
+rootRouter.use("api/v1/types", typeRouter)
+rootRouter.use("api/v1/pets", petRouter)
 rootRouter.use("./api/v1/surrenderRouter.js", surrenderRouter)
 
 rootRouter.use("/", clientRouter)
