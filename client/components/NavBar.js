9e0fcee77c10bf24e5f39c6fc8247c30f09934b3
kpartain <karalynnleann@gmail.com>
2021-03-26 09:23:22 -0700

    Add HTML and Foundation elements, revise formatting to meet standard
    Added functionality updates to ensure form upon submission will persist data as expected to tables.

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -4,26 +4,89 @@ import LandingPage from "./LandingPage"
 import IndexPage from "./IndexPage"
 import ShowPage from "./ShowPage"
 import SurrenderForm from "./SurrenderForm"
+
 const NavBar = props => {
-return (
-  <div>
-  <div className="navbar top">
-    <a href="/pets">HOME</a> || 
-    <a href="/pets/pigs">Pigs</a> ||
-    <a href="/pets/bunnies">Bunnies</a> || 
-    <a href="/pets/unicorns">Unicorns</a> ||
-    <a href="/adoptions/new">Surrender an Animal</a>
-  </div>
-       <Switch>
-       <Route exact path="/" component={LandingPage} />
-       <Route exact path="/pets" component={LandingPage} />
-       <Route exact path="/pets/:type" component={IndexPage} />
-       <Route exact path="/adoptions/new" component={SurrenderForm} />
-       <Route exact path="/pets/pigs/:id" component={ShowPage} /> 
-       <Route exact path="/pets/bunnies/:id" component={ShowPage} /> 
-       <Route exact path="/pets/unicorns/:id" component={ShowPage} />
-     </Switch>
-     </div>
+  return (
+    <div>
+      <nav id="navbar" data-sticky-container>
+        <div className="top-bar" data-sticky data-options="marginTop:0;" id="navbar">
+          <div className="top-bar-left">
+            <ul className="menu">
+              <li>
+                <a href="/pets">
+                  <img
+                    src="https://i.ibb.co/PW2WY8S/code-club-adopt-logo.png"
+                    alt="code-club-adopt-logo"
+                    className="photo"
+                  />
+                </a>
+              </li>
+              <li>
+                <a href="/pets/pigs">
+                  <img
+                    src="https://i.ibb.co/BtCjGcb/pig-logo.png"
+                    alt="pig-logo"
+                    className="photo"
+                  />
+                  Pigs
+                </a>
+              </li>
+              <li>
+                <a href="/pets/bunnies">
+                  <img
+                    src="https://i.ibb.co/dk7L8jQ/bunny-logo.png"
+                    alt="bunny-logo"
+                    className="photo"
+                  />
+                  Bunnies
+                </a>
+              </li>
+              <li>
+                <a href="/pets/unicorns">
+                  <img
+                    src="https://i.ibb.co/2dbxds2/unicorn-logo.png"
+                    alt="unicorn-logo"
+                    className="photo"
+                  />
+                  Unicorns
+                </a>
+              </li>
+              <li>
+                <a href="/adoptions/new">
+                  <img
+                    src="https://i.ibb.co/9wT7zm2/surrender-logo.png"
+                    alt="surrender-logo"
+                    className="photo"
+                  />
+                  Surrender
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className="grid-container">
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/pets" component={LandingPage} />
+          <Route exact path="/pets/:type" component={IndexPage} />
+          <Route exact path="/adoptions/new" component={SurrenderForm} />
+          <Route exact path="/pets/pigs/:id" component={ShowPage} />
+          <Route exact path="/pets/bunnies/:id" component={ShowPage} />
+          <Route exact path="/pets/unicorns/:id" component={ShowPage} />
+        </Switch>
+      </div>
+
+      <footer data-sticky-container>
+        <div className="footer">
+          <ul className="inline-center">
+            <li>| &copy; Code-Club, Launch Academy Group Project March 2021 |</li>
+            <li>GitHub contributors: wang-chenxi, kpartain, mattacognition, PatrickStevenson</li>
+          </ul>
+        </div>
+      </footer>
+    </div>
   )
 }
+
 export default NavBar
--- server/routes/api/v1/surrenderRouter.js
+++ server/routes/api/v1/surrenderRouter.js
@@ -1,21 +1,24 @@
 import express from "express"
 import Surrender from "../../../models/Surrender.js"
+
 const surrenderRouter = new express.Router()
+
 surrenderRouter.get("/", (req, res) => {
-    res.render()
+  res.render()
 })
 surrenderRouter.post("/", async (req, res) => {
-    try {
-        const newForm = req.body
-        const surrender = new Surrender(newForm);
-        if (await surrender.saveSurrender()) {
-            res.status(201).json({})
-        } else {
-            res.status(422).json({})
-        }
-    } catch (error) {
-        console.error(error)
-        res.status(422)
+  try {
+    const newForm = req.body
+    const surrender = new Surrender(newForm)
+    if (await surrender.saveSurrender()) {
+      res.status(201).json({})
+    } else {
+      res.status(422).json({})
     }
+  } catch (error) {
+    console.error(error)
+    res.status(422)
+  }
 })
+
 export default surrenderRouter
